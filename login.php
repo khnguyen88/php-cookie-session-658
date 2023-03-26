b7c9ab9c81115a061a190f3850cd3deafa0a7d6a
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2023-03-26 01:25:15 -0400

    Added php script logic for loading specific display mode stylesheet based on displayMode cookie value.

--- login.php
+++ login.php
@@ -1,4 +1,3 @@
-<!-- Following demo + W3School, how to set cookies -->
 <?php 
     session_start();
     if(!isset($_SESSION["loggedIn"])){
@@ -26,6 +25,20 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+        <?php
+            if(isset($_COOKIE["displayMode"])){
+                if($_COOKIE["displayMode"] == "light"){
+                    echo '<link rel="stylesheet" href="./styles/light.css">';
+                }
+                if($_COOKIE["displayMode"] == "dark"){
+                    echo '<link rel="stylesheet" href="./styles/dark.css">';
+                }
+
+            }
+            else{
+                echo '<link rel="stylesheet" href="./styles/light.css">';
+            }
+        ?>
     </head>
 
     <body>
